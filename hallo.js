a81c5c53d9a49281c62e2deaecbd2e2ea992bf55
Dian <dian.gustara@gmail.com>
2023-07-13 19:18:53 +0700

    Nama

--- hallo.js
+++ hallo.js
@@ -1,5 +1,8 @@
-var namadepan = "Dian";
-var namabelakang = " Gustara ";
-var namalengkap = namadepan + namabelakang;
-console.log("full Name :", namalengkap);
 
+
+var namadepan = ("Masukan Nama Depan:");
+var namabelakang = ("Masukan Nama belakang:");
+var tahunlahir = ("Masukan Tahun Lahir :");
+
+console.log("Nama Lengkap :", namadepan,namabelakang);
+console.log("Tahun Lahir :", tahunlahir);
